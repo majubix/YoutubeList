0989d6889cb8da25db0db86dd48a955657e4cb96
da Rocha <maria.da.rocha@accenture.com>
2025-04-25 17:01:08 -0300

    lista youtube

---
+++ list.py
@@ -0,0 +1,22 @@
+from googleapiclient.discovery import build
+API_KEY = 'AIzaSyBNeKHZ1pCylcWDHSurWugdQGm5uiwWon8'
+
+youtube = build('youtube','v3', developerKey=API_KEY)
+
+CANAL_ID = 'UC__dSYOfZ8QnGLiYes82D_A'
+
+def listar_subscript():
+    request = youtube.subscriptions().list(
+        part='snippet',
+        channelId=CANAL_ID,
+        maxResults=20,
+        order='relevance'
+    )
+    response = request.execute()
+
+    for item in response.get('items',[]):
+        nome = item['snippet']['title']
+        canal = item['snippet']['resourceId']['channelId']
+        print(f'{nome} - Canal ID: {canal}')
+
+listar_subscript()
